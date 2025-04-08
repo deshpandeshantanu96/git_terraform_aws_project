4c6e20d4ae4888e087a793f24e1b6055fa90db58
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-08 11:02:18 +0530

    Added data

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -29,7 +29,7 @@ resource "aws_lb_target_group" "dummy" {
   name        = "dummy-target"
   port        = 80
   protocol    = "HTTP"
-  vpc_id      = var.alb_inputs.vpc_id
+  vpc_id      = var.vpc_id
   target_type = "ip"
 }
 
