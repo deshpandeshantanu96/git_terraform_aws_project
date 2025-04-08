9a3aa052c5199e4a898b012aff47814707c51bb3
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-08 10:37:19 +0530

    Added data

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "VPC ID created by the vpc module"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs created by the vpc module"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "bastion_public_ip" {
+  description = "Public IP of the bastion host"
+  value       = module.bastion.bastion_public_ip
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the ALB"
+  value       = module.alb.alb_dns_name
+}
