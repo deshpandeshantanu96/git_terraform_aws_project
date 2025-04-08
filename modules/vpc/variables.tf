8375bc5329b3995cbd1ebf1432c4b726a4637912
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-08 16:30:03 +0530

    Added data

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -2,14 +2,18 @@
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
 }
 
 variable "private_subnet_cidrs" {
-  description = "List of CIDR blocks for private subnets"
+  description = "List of private subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.2.0/24", "10.0.3.0/24"]
 }
+
 
 variable "public_subnet_count" {
   description = "Number of public subnets to create"
