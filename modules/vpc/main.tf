58003b22469293ddba106c9abe0b69e1c011540d
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-08 10:58:13 +0530

    Added data

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,7 +12,6 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   map_public_ip_on_launch = true
-  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
