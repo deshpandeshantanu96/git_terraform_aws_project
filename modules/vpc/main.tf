1867eac8236bdcb88f06573b7181dd53ac15654b
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-08 15:31:58 +0530

    Added data

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,6 +11,7 @@ resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)  # Different AZs
   map_public_ip_on_launch = true
 }
 
