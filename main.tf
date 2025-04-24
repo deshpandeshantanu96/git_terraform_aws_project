9b1fd276a932dbafe83f9020f6dd6558297e6d7c
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-24 11:25:19 +0530

    Updated the workflow

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "internal_lb_sg" {
   description = "Security Group for Internal Load Balancer"
   vpc_id      = module.vpc.vpc_id
 
-  ingress {AmazonEKSWorkerNodePolicy
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
