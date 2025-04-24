60364c3cc673a222b7c0c6ecd0c15f8bcac69336
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-24 18:42:34 +0530

    Changed the eks terraform

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -10,3 +10,58 @@ resource "aws_eks_cluster" "this" {
 
   version = var.cluster_version
 }
+
+# modules/eks/main.tf
+
+# Security group for EKS control plane (created by AWS, but we add rules)
+resource "aws_security_group_rule" "eks_ingress_nodes" {
+  description              = "Allow worker nodes to communicate with EKS API"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+  source_security_group_id = aws_security_group.eks_nodes.id
+}
+
+# modules/eks/main.tf
+
+# Security group for worker nodes
+resource "aws_security_group" "eks_nodes" {
+  name        = "${var.cluster_name}-node-sg"
+  description = "Security group for EKS worker nodes"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-node-sg"
+  }
+}
+
+# Allow EKS control plane to communicate with worker nodes
+resource "aws_security_group_rule" "node_ingress_eks" {
+  description              = "Allow EKS control plane to communicate with worker nodes"
+  type                     = "ingress"
+  from_port                = 10250
+  to_port                  = 10250
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+}
+
+# Optional: Allow SSH access (if debugging is needed)
+resource "aws_security_group_rule" "node_ingress_ssh" {
+  description       = "Allow SSH access to worker nodes"
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_nodes.id
+  cidr_blocks       = ["10.0.0.0/16"]  # Restrict to your VPC or specific IPs
+}
--- modules/eks/node_group.tf
+++ modules/eks/node_group.tf
@@ -1,8 +1,16 @@
+# modules/eks/main.tf
+
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.subnet_ids
+  instance_types  = ["t2.medium"]
+  
+  # Attach the node security group
+  remote_access {
+    ec2_ssh_key = "terraform_application_key"
+  }
 
   scaling_config {
     desired_size = 2
@@ -10,7 +18,6 @@ resource "aws_eks_node_group" "this" {
     min_size     = 1
   }
 
-  remote_access {
-    ec2_ssh_key = "terraform_application_key" # Must exist in AWS
-  }
+  # Associate the node security group
+  vpc_security_group_ids = [aws_security_group.eks_nodes.id]
 }
