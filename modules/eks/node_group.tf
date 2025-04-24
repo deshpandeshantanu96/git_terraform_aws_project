45f1f88021fc74268b45e8bbe7ecd5af462447c3
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-24 18:56:11 +0530

    Changed the eks terraform

--- modules/eks/node_group.tf
+++ modules/eks/node_group.tf
@@ -18,6 +18,25 @@ resource "aws_eks_node_group" "this" {
     min_size     = 1
   }
 
-  # Associate the node security group
+}
+
+resource "aws_launch_template" "eks_nodes" {
+  name_prefix   = "${var.cluster_name}-node-"
+  instance_type = "t2.medium"  # Overrides instance_types in node group
+  key_name      = "terraform_application_key"  # For SSH access
+
+  # Assign the custom security group
   vpc_security_group_ids = [aws_security_group.eks_nodes.id]
+
+  # Required for EKS nodes
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.cluster_name}-worker-node"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
