1e4f99b1b80dd3c8068214cd67a9554c2e0506f1
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-25 13:42:00 +0530

    Updated the eks code

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,14 +1,36 @@
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Action    = "sts:AssumeRole"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_role_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_role.name
+}
+
 resource "aws_eks_cluster" "this" {
-  name     = var.cluster_name
+  name     = var.eks_config.cluster_name
   role_arn = var.role_arn
-  
+
   vpc_config {
     subnet_ids = var.subnet_ids
-    endpoint_private_access = true
-    endpoint_public_access  = true
+    endpoint_public_access = true
   }
 
-  version = var.cluster_version
+  version = var.eks_config.cluster_version
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_role_policy_attachment
+  ]
 }
 
 # modules/eks/main.tf
