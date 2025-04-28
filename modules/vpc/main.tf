b0142aad81d6730898b6d7ae1fab53e5bb4b98dd
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-28 12:42:33 +0530

    Code updated

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -46,6 +46,7 @@ resource "aws_subnet" "private" {
   }
 }
 
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   tags = {
@@ -58,29 +59,50 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
-
+# Elastic IP for NAT Gateway
 resource "aws_eip" "nat" {
   domain = "vpc"
+  tags = {
+    Name = "nat-gateway-eip"
+  }
 }
 
-resource "aws_nat_gateway" "this" {
+# NAT Gateway in first public subnet
+resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id  # Reference the subnet directly
-  
-  depends_on = [aws_internet_gateway.main]
+  subnet_id     = aws_subnet.public[0].id  # Placed in first public subnet
+
+  tags = {
+    Name = "main-nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
 }
 
-# Then create private route table
+# Private Route Table
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
+    nat_gateway_id = aws_nat_gateway.nat.id
   }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public" {
+  count          = var.public_subnet_count
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Associate Private Subnets with Private Route Table
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
