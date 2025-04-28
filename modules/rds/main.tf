5669467cb67c312eaafa99eb9a5c53e94e33daa4
Shantanu <deshpandeshantanu96@gmail.com>
2025-04-28 23:47:42 +0530

    Updated dns as well load balancer code for eks, also rds

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,5 +1,5 @@
-resource "aws_db_subnet_group" "rds_subnet_group_aws" {
-  name       = "rds-subnet-group"
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group-aws"
   subnet_ids = var.subnet_ids
 
   tags = {
